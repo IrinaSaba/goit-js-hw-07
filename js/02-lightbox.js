6ae5feb492f0ac0a18b3337221f0c18f078bf9c5
Irina Sabaliauskiene <sabaliauskieneirina@gmail.com>
2021-12-22 13:14:10 +0100

    2 exo ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const refs = {
+  galeryOne: document.querySelector(".gallery"),
+};
+const readyGal = createGalery(galleryItems);
+
+refs.galeryOne.insertAdjacentHTML("afterbegin", readyGal);
+
+// console.log(createGalery(galleryItems));
+function createGalery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: "250ms",
+  captionsData: "alt",
+});
+lightbox.close();
